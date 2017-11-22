53bb266bf4659f62a5f47dc7c05aaf242be0bed3
Phala <pphala@informatica.com>
2017-11-23 00:23:43 +0530

    Encoding catagorical data

--- 1. Data Preprocessing/data_pre_param.py
+++ 1. Data Preprocessing/data_pre_param.py
@@ -20,13 +20,17 @@ y = dataset.iloc[:,-1:].values #Simple arg2 is 3 instead of -1:
 
 
 #Handling missing data
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import Imputer, OneHotEncoder
 impute = Imputer(missing_values="NaN", strategy="mean", axis=0) #same as Imputer()
 impute = impute.fit(x[:,1:])
 x[:,1:] = impute.transform(x[:,1:])
-print(x)
-print(y)
 
+#Encoding catagorical data
+from sklearn.preprocessing import LabelEncoder 
+le_x = LabelEncoder()
+x[:,0] = le_x.fit_transform(x[:,0])
+ohe = OneHotEncoder(categorical_features = [0])
+x = ohe.fit_transform(x).toarray()
 
-
-
+le_y = LabelEncoder()
+y = le_x.fit_transform(y)
